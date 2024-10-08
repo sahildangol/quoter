6415224ccf42a397951fec6b5e237007afeb8db1
Sahil <sahildangol2004@gmail.com>
2024-10-08 14:52:26 +0545

    type1

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect,useState } from "react/cjs/react.production.min";
+import { useEffect, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 
 import Form from "@components/Form";
